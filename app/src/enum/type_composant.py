da4260f678a963932135989117a4a0013d1bde7d
Ad2rien5 <seach.adrien@gmail.com>
2025-06-18 16:04:35 +0200

    Trad on ComponentType enum attribute

--- app/src/enum/type_composant.py
+++ app/src/enum/type_composant.py
@@ -2,12 +2,12 @@ from enum import Enum
 
 
 class ComponentType(Enum):
-    NOEUD = 1
-    RESISTANCE = 2
-    GENERATEUR_TENSION = 3
-    GENERATEUR_COURANT = 4
-    INDUCTANCE = 5
-    CONDENSATEUR = 6
-    INTERUPTEUR = 7
+    NODE = 1
+    RESISTOR = 2
+    VOLTAGE_GENERATOR = 3
+    CURRENT_GENERATOR = 4
+    INDUCTOR = 5
+    CAPACITOR = 6
+    SWITCH = 7
     DIODE = 8
     TRANSISTOR = 9
