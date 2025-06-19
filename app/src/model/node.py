4294d5eaa4b1b7679bbddc0b55afbacf06b8e946
Ad2rien5 <seach.adrien@gmail.com>
2025-06-19 12:27:56 +0200

    ADD node.py

---
+++ app/src/model/node.py
@@ -0,0 +1,27 @@
+from app.src.enum.component_type import ComponentType
+from app.src.model.component import Component
+
+
+class Node(Component):
+
+    def __init__(self, name: str, **components: Component):
+        self.names = name
+        self.typeC = ComponentType.NODE
+        self.components: Component = []
+
+        for compo in components:
+            self.components.append(compo)
+
+    def __contains__(self, item):
+
+        match item:
+            case str():
+                return item in [i.name for i in self.components]
+
+            case Component():
+                return item in [i for i in self.components]
+
+        raise ValueError(f"Cannot compare Component object with {type(item)}.")
+
+    def add(self, component: Component):
+        self.components.append(component)
